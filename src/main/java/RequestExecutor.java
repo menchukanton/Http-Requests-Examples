085854afc06896a7cf70be7a4b50212c343ea891
menchukanton <menchukanton@gmail.com>
2018-11-06 12:52:39 +0200

    Created examples of work with GET & POST http request. Also response handling(JSONPath using).

---
+++ src/main/java/RequestExecutor.java
@@ -0,0 +1,39 @@
+import com.jayway.jsonpath.JsonPath;
+
+import java.io.IOException;
+
+public class RequestExecutor {
+
+
+
+    public static void main(String[] args) throws IOException {
+        String domain = "https://noName.testrail.io";
+
+        Requests requests = new Requests();
+
+        String getStringResponse  = requests.GET(domain + "/index.php?/api/v2/get_case/727");
+        String postStringResponse = requests.POST(domain + "/index.php?/api/v2/add_run/4","{\"name\":\"Test Run NEW Name.\"}");
+
+        System.out.println("GET request: " + getStringResponse);
+        System.out.println("ID: " + getJsonValue(getStringResponse,"$.id"));
+        System.out.println("Title: " + getJsonValue(getStringResponse,"$.title"));
+        System.out.println("Estimate forecast: " + getJsonValue(getStringResponse,"$.estimate_forecast"));
+
+        System.out.println("\n");
+
+        System.out.println("POST request: " + postStringResponse);
+        System.out.println("ID: " + getJsonValue(postStringResponse,"$.id"));
+        System.out.println("Name: " + getJsonValue(postStringResponse,"$.name"));
+        System.out.println("Description: " + getJsonValue(postStringResponse,"$.description"));
+        System.out.println("Created on: " + getJsonValue(postStringResponse,"$.created_on"));
+        System.out.println("Project ID: " + getJsonValue(postStringResponse,"$.project_id"));
+    }
+
+    private static String getJsonValue(String string, String pathToValue){
+        try{
+            return JsonPath.parse(string).read(pathToValue).toString();
+        }catch (NullPointerException e){
+            return "null";
+        }
+    }
+}
---
+++ src/main/java/Requests.java
@@ -0,0 +1,61 @@
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.HttpClients;
+
+public class Requests {
+
+    private HttpClient client = HttpClients.custom().build();
+    private HttpUriRequest request;
+    private HttpResponse httpResponse;
+    private String response = "";
+
+    String GET(String uri){
+        try {
+            request = RequestBuilder.get()
+                    .setUri(uri)
+                    .setHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                    .setHeader(HttpHeaders.AUTHORIZATION,"Basic YW50b24ubWVuY2h1a0B0YXAudWE6UzRiS2xTSHpRV1M4bmFqTmlReFotelBxWXdJRE1RalJsZ0d6SnlBQVI=")
+                    .build();
+
+            httpResponse = client.execute(request);
+            response = new BasicResponseHandler().handleResponse(httpResponse);
+
+        //printing method name and request status
+        System.out.println( "Status info:\n" +
+                Thread.currentThread().getStackTrace()[1].getMethodName() + " " +
+                httpResponse.getStatusLine());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return e.getMessage();
+        }
+        return response;
+    }
+
+    String POST(String uri, String parameters) {
+        try {
+            request = RequestBuilder.post()
+                    .setUri(uri)
+                    .setHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                    .setHeader(HttpHeaders.AUTHORIZATION,"Basic YW50b24ubWVuY2h1a0B0YXAudWE6UzRiS2xTSHpRV1M4bmFqTmlReFotelBxWXdJRE1RalJsZ0d6SnlBQVI=")
+                    .setEntity(new StringEntity(parameters))
+                    .build();
+
+            httpResponse = client.execute(request);
+            response = new BasicResponseHandler().handleResponse(httpResponse);
+
+        //printing method name and request status
+            System.out.println( "Status info:\n" +
+                    Thread.currentThread().getStackTrace()[1].getMethodName() + " " +
+                    httpResponse.getStatusLine());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return e.getMessage();
+        }
+        return response;
+    }
+}
